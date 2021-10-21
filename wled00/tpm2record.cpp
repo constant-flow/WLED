ece8b000487ec6761039d4f8c759200e2f87c552
constantin wolf <constantin.wolf@pwc.com>
2021-10-21 17:55:47 +0200

    clean up, stub to process of response data

--- wled00/tpm2record.cpp
+++ wled00/tpm2record.cpp
@@ -1,10 +1,16 @@
 #include "wled.h"
 
+// reference spec: https://gist.github.com/jblang/89e24e2655be6c463c56
+// - A packet contains any data of the TPM2 protocol, it
+//     starts with `TPM2_START` and ends with `TPM2_END`
+// - A frame contains the visual data (the LEDs color's) of one moment
+
 // --- CONSTANTS ---
 #define TPM2_START      0xC9
 #define TPM2_DATA_FRAME 0xDA
 #define TPM2_COMMAND    0xC0
 #define TPM2_END        0x36
+#define TPM2_RESPONSE   0xAA
 
 // infinite loop of animation
 #define RECORDING_REPEAT_LOOP -1
@@ -20,41 +26,21 @@ uint32_t msFrameDelay     = 33; // time between frames
 int32_t  recordingRepeats = RECORDING_REPEAT_LOOP;
 unsigned long lastFrame   = 0;
 
-// scrolls to next frame, returns false when no further data is available
-bool ignoreDataUntilNextFrame()
-{
+// skips until a specific byte comes up
+void skipUntil(uint8_t byteToStopAt) {
   uint8_t rb = 0;
-  do
-  {
-    rb = recordingFile.read();
-    DEBUG_PRINT("ignore until next frame: ");
-    DEBUG_PRINTLN(rb);
-  } while (recordingFile.available() && rb != TPM2_START);
-  if (!recordingFile.available())
-    return false;
-  return true;
+  do { rb = recordingFile.read(); }
+  while (recordingFile.available() && rb != byteToStopAt);
 }
 
-void gotoEndOfFrame()
+void skipUntilNextPacket() { skipUntil(TPM2_START); }
+
+void skipUntilEndOfPacket()
 {
   uint8_t rb = 0;
-  do
-  {
-    if (rb != 0)
-      DEBUG_PRINTLN("WARNING: Frame was expected to be at end");
-    rb = recordingFile.read();
-    DEBUG_PRINT("Go to end of frame: ");
-    DEBUG_PRINT(rb);
-  } while (recordingFile.available() && rb != TPM2_END);
-  DEBUG_PRINT(" (Data left: ");
-  DEBUG_PRINT(recordingFile.available());
-  DEBUG_PRINTLN(")");
-}
-
-void readCommandData()
-{
-  DEBUG_PRINTLN("readCommandData: not implemented yet");
-  ignoreDataUntilNextFrame();
+  do { rb = recordingFile.read(); } 
+  while (recordingFile.available() && rb != TPM2_END);
+  skipUntil(TPM2_END);
 }
 
 void getNextColorData(uint8_t data[])
@@ -62,106 +48,41 @@ void getNextColorData(uint8_t data[])
   data[0] = recordingFile.read();
   data[1] = recordingFile.read();
   data[2] = recordingFile.read();
-  DEBUG_PRINT("read color: ");
-  DEBUG_PRINT(data[0]);
-  DEBUG_PRINT(" ");
-  DEBUG_PRINT(data[1]);
-  DEBUG_PRINT(" ");
-  DEBUG_PRINT(data[2]);
-  DEBUG_PRINTLN("");
-  data[3] = 0; // TODO add RGBW mode to TPM2: colorChannels = 4
+  data[3] = 0; // TODO add RGBW mode to TPM2
 }
 
-uint16_t getNextFrameLength()
+uint16_t getNextPacketLength()
 {
-  if (!recordingFile.available())
-    return 0;
+  if (!recordingFile.available()) { return 0; }
   uint8_t highbyte_size = recordingFile.read();
   uint8_t lowbyte_size = recordingFile.read();
   uint16_t size = highbyte_size << 8 | lowbyte_size;
-  DEBUG_PRINT("Payload size: ");
-  DEBUG_PRINT(size);
-  DEBUG_PRINT(" (");
-  DEBUG_PRINT(highbyte_size);
-  DEBUG_PRINT(",");
-  DEBUG_PRINT(lowbyte_size);
-  DEBUG_PRINTLN(")");
   return size;
 }
 
-void playNextRecordingFrame()
+void processCommandData()
 {
-  DEBUG_PRINT("=== Load next Frame (Data left: ");
-  DEBUG_PRINT(recordingFile.available());
-  DEBUG_PRINTLN(")");
+  DEBUG_PRINTLN("processCommandData: not implemented yet");
+  skipUntilNextPacket();
+}
 
-  //If recording reached end loop or stop playback
-  if (!recordingFile.available())
-  {
-    if (recordingRepeats == RECORDING_REPEAT_LOOP)
-    {
-      recordingFile.seek(0);
-      DEBUG_PRINTLN("Repeat (loop)");
-    }
-    else if (recordingRepeats > 0)
-    {
-      recordingFile.seek(0);
-      recordingRepeats--;
-      DEBUG_PRINT("Repeat for ");
-      DEBUG_PRINT(recordingRepeats);
-      DEBUG_PRINTLN(" loops");
-    }
-    else
-    {
-      DEBUG_PRINT("Repeats: ");
-      DEBUG_PRINTLN(recordingRepeats);
-      DEBUG_PRINTLN("STOP RECORDING PLAYBACK");
-      uint8_t mode = REALTIME_MODE_INACTIVE;
-      realtimeLock(0, mode);
-      recordingFile.close();
-      return;
-    }
-  }
+void processResponseData()
+{
+  DEBUG_PRINTLN("processResponseData: not implemented yet");
+  skipUntilNextPacket();
+}
 
-  // scan to next TPM2 Frame start, should be the first attempt
-  uint8_t rb = 0;
-  do
-  {
-    rb = recordingFile.read();
-    DEBUG_PRINT("Read new frame: ");
-    DEBUG_PRINTLN(rb);
-  } while (recordingFile.available() && rb != TPM2_START);
-  if (!recordingFile.available())
-    return;
+void processFrameData()
+{
+  uint16_t packetLength = getNextPacketLength(); // opt-TODO maybe stretch recording to available leds
 
-  rb = recordingFile.read();
-  DEBUG_PRINT("check if data or command: ");
-  DEBUG_PRINTLN(rb);
-
-  while (rb == TPM2_COMMAND)
-  {
-    readCommandData(); //it's assumed all control data is stored in the beginning
-    rb = recordingFile.read();
-    DEBUG_PRINT("scan for command or data: ");
-    DEBUG_PRINTLN(rb);
-  }
-
-  if (rb != TPM2_DATA_FRAME)
-  {
-    DEBUG_PRINTLN("WARNING: RECEIVED UNEXPECTED DATA");
-    DEBUG_PRINTLN(rb);
-    return;
-  }
-
-  uint16_t frameLength = getNextFrameLength(); // opt-TODO maybe stretch recording to available leds
-
-  for (uint16_t i = 0; i < frameLength / colorChannels; i++)
+  for (uint16_t i = 0; i < packetLength / colorChannels; i++)
   {
     getNextColorData(colorData);
     setRealtimePixel(i, colorData[0], colorData[1], colorData[2], colorData[3]);
   }
 
-  gotoEndOfFrame();
+  skipUntilEndOfPacket();
 
   strip.show();
   // tell ui we are playing the recording right now
@@ -171,14 +92,70 @@ void playNextRecordingFrame()
   lastFrame = millis();
 }
 
+void processUnknownData(uint8_t data)
+{
+  DEBUG_PRINT("processUnknownData - received:");
+  DEBUG_PRINTLN(data);
+  skipUntilNextPacket();
+}
+
+bool stopBecauseAtTheEnd() {
+  //If recording reached end loop or stop playback
+  if (!recordingFile.available())
+  {
+    if (recordingRepeats == RECORDING_REPEAT_LOOP)
+    {
+      recordingFile.seek(0); // go back the beginning of the recording
+    }
+    else if (recordingRepeats > 0)
+    {
+      recordingFile.seek(0); // go back the beginning of the recording
+      recordingRepeats--;
+      DEBUG_PRINT("Repeat recordind again for:");
+      DEBUG_PRINTLN(recordingRepeats);      
+    }
+    else
+    {      
+      uint8_t mode = REALTIME_MODE_INACTIVE;
+      realtimeLock(10, mode);
+      recordingFile.close();
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// scan and forward until next frame was read (this will process commands)
+void playNextRecordingFrame()
+{
+  if(stopBecauseAtTheEnd()) return;
+
+  uint8_t rb = 0; // last read byte from file
+
+  // scan to next TPM2 packet start, should be the first attempt
+  do { rb = recordingFile.read(); } 
+  while (recordingFile.available() && rb != TPM2_START);  
+  if (!recordingFile.available()) { return; }
+
+  // process everything until (including) the next frame data
+  while(true)
+  {
+    rb = recordingFile.read();
+    if     (rb == TPM2_COMMAND)    processCommandData();     
+    else if(rb == TPM2_RESPONSE)   processResponseData();
+    else if(rb != TPM2_DATA_FRAME) processUnknownData(rb);
+    else {
+      processFrameData();
+      break;      
+    }
+  }
+}
+
 void handlePlayRecording()
 {
-  if (realtimeMode != REALTIME_MODE_TPM2RECORD)
-    return;
-  if ( millis() - lastFrame < msFrameDelay)
-    return; // TODO limit to correct framerate
-  DEBUG_PRINT("recording time passed: ");
-  DEBUG_PRINTLN( millis() - lastFrame);
+  if (realtimeMode != REALTIME_MODE_TPM2RECORD) return;
+  if ( millis() - lastFrame < msFrameDelay)     return;
   playNextRecordingFrame();
 }
 
